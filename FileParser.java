5cf9b4fa4372c9b6bc18626181822f9d70f94254
kna3 <kna3@binghamton.edu>
2019-09-19 04:41:51 -0400

    Parses and adds the information of the Course and Student and return both as two different lists

--- FileParser.java
+++ FileParser.java
@@ -1,90 +1,80 @@
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
-//import Student.Level;
-
 public class FileParser {
-	static Validate obj = new Validate();
+	/**
+     * Parses the courseinfo.txt file and adds information to the CourseList
+     */
+	public List<Courses> ParseCourseFile(List<String> list) {
 
-	public void ParseCourseFile(String file) {
-		
+		List<Courses> courseList = new ArrayList<>();
 		try {
-				
-		List<String> list = new ArrayList<String>();
-		FileProcessor fileobj = new FileProcessor();
-		list = fileobj.readLine(file);
+			Iterator<String> itr = list.iterator();
+			String line = null;
+			while (itr.hasNext()) {
+				line = itr.next().toString();
+				int capacityIndex = line.indexOf("CAPACITY:") + 9;
+				int timingIndex = line.indexOf("CLASS_TIMING:") + 13;
+				char c = line.charAt(0);
+				int cap = Integer.parseInt(line.substring(capacityIndex, capacityIndex + 2));
+				int time = Integer.parseInt(line.substring(timingIndex, timingIndex + 1));
+
+				Courses courses = new Courses();
+				courses.setCapacity(cap);
+				courses.setClassTimings(time);
+				courses.setCourseName(c);
+
+				courseList.add(courses);
+			}
+		} catch (Exception e) {
+			System.out.println("Failed to Parse");
+			System.out.println(e.getMessage());
+			System.exit(0);
+		}
+
+		return courseList;
+	}
+	/**
+     * Parses the student.txt file and adds information to the studentList
+     */
+	public List<Student> ParseStudentFile(List<String> list) {
 		
-		Iterator<String> itr =list.iterator();
-		String line = null;
-		while(itr.hasNext()) {
-		     line = itr.next().toString();
-			 int capacityIndex = line.indexOf("CAPACITY:")+ 9;
-	    	 int timingIndex = line.indexOf("CLASS_TIMING:")+ 13;
-	    	 char c = line.charAt(0);
-	    	 int cap = Integer.parseInt(line.substring(capacityIndex,capacityIndex+2));
-	    	 int time = Integer.parseInt(line.substring(timingIndex,timingIndex+1));
-	    	 
-	    	 Courses courses=new Courses();
-	    	 courses.setCapacity(cap);
-	    	 courses.setClassTimings(time);
-	    	 courses.setCourseName(c);
-	    	 obj.getCourseList().add(courses);
-	      
-	
-		}
-	}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-	
-	public void ParseStudentFile(String file) {
+		List<Student> studentList = new ArrayList<>();
 		try {
-			
-		List<String> preferences = new ArrayList<String>();
-		List<String> list = new ArrayList<String>();
-		FileProcessor fileobj = new FileProcessor();
-		list = fileobj.readLine(file);
-		
-		Iterator<String> itr =list.iterator();
-		String line = null;
-		int studentId;
-		while(itr.hasNext()) {
-		     line = itr.next();
-		    // System.out.println("line :" + itr.next());
-		     //System.out.println("line :" + line);
-		     String id = line.substring(0, 3);
-	         System.out.println("id :" + id);
-	         studentId = (Integer.parseInt(id));
-			 System.out.println("id1 :" + studentId);
-		     String prefer = (line.substring(4,21));
-		     String[] pref = prefer.split(",");
-			 for (String a : pref) {
-	        
-	   	     preferences = Arrays.asList(a);
-			 }
-	   	     String level="";
-	   	     level = (line.substring(23, line.length()));
-	   	    // System.out.println("id 2 :" + id);  
-	   	     Student student=new Student();
-	   	     student.setStudentId(studentId);
-	   	     student.setPreferences(preferences);
-	         student.setLevel(level);
-	    	 obj.getStudentList().add(student);
-	    	 
+			Iterator<String> itr = list.iterator();
+			String line = null;
+			int studentId;
+			while (itr.hasNext()) {
+				List<String> preferences = new ArrayList<String>();
+				line = itr.next();
+				String id = line.substring(0, 3);
+				studentId = (Integer.parseInt(id));
+				String prefer = (line.substring(4, 21));
+				String[] pref = prefer.split(",");
+				for (String a : pref) {
+
+					preferences.add(a);
+				}
+				String level = "";
+				level = (line.substring(23, line.length()));
+				Student student = new Student();
+				student.setStudentId(studentId);
+				student.setPreferences(preferences);
+				student.setLevel(level);
+				studentList.add(student);
+
+			}
 		}
-	}		 
-		
-		catch(Exception e){
-			
-			System.exit(1);	
+
+		catch (Exception e) {
+			System.out.println("Failed to Parse");
+			System.out.println(e.getMessage());
+			System.exit(0);
 		}
-		obj.test();
+		return studentList;
+		
 	}
-		
+
 }
-
 
